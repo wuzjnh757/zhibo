e70ed8f77b79cccf08f1775177fc7a35abd98973
wuzjnh757 <wuzjnh757@gmail.com>
2026-08-05 13:05:14 +0800

    Update generateM3U.js

--- generateM3U.js
+++ generateM3U.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 
 async function generateM3U() {
   try {
-    const res = await axios.get('http://api.hclyz.com:81/multi');
-    const data = res.data;
+    const res = await axios.get('http://api.hclyz.com:81/json.txt');
+    console.log('API 返回数据:', res.data); // 调试输出
 
+    const data = res.data;
     let m3u = '#EXTM3U\n';
+
     data.forEach(item => {
       if (item.url && (item.url.includes('.m3u8') || item.url.includes('.flv'))) {
         m3u += `#EXTINF:-1, ${item.name}\n${item.url}\n`;
